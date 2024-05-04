0e9bce783a04abc20178723342467be5ffb64a80
Taufan Ali <126685412+taufanAli65@users.noreply.github.com>
2024-05-04 20:42:25 +0700

    update sampe praktikum 5

---
+++ mdc_100_series/lib/login.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Image.asset('assets/diamond.png'),
+                const SizedBox(height: 16.0),
+                const Text('SHRINE'),
+              ],
+            ),
+            const SizedBox(height: 120.0),
+            TextField(
+              controller: _usernameController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            const SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            OverflowBar(
+              alignment: MainAxisAlignment.end,
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                   TextButton(
+                    child: const Text('CANCEL'),
+                    onPressed: () {
+                        _usernameController.clear();
+                        _passwordController.clear();
+                    },
+                  ),
+                  // TODO: Add an elevation to NEXT (103)
+                  // TODO: Add a beveled rectangular border to NEXT (103)
+                  ElevatedButton(
+                    child: const Text('NEXT'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
